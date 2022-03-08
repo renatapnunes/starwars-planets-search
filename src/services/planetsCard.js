b192f30c97ac9f4423d34d6f235c1633326a8671
Renata Nunes <renatanunes231@yahoo.com.br>
2022-03-08 03:23:20 -0300

    Add images for README

---
+++ src/services/planetsCard.js
@@ -0,0 +1,25 @@
+import Alderaan from '../images/planets/alderaan.png';
+import Bespin from '../images/planets/bespin.png';
+import Coruscant from '../images/planets/coruscant.png';
+import Dagobah from '../images/planets/dagobah.png';
+import Endor from '../images/planets/endor.png';
+import Hoth from '../images/planets/hoth.png';
+import Kamino from '../images/planets/kamino.png';
+import Naboo from '../images/planets/naboo.png';
+import Tatooine from '../images/planets/tatooine.png';
+import Yavin4 from '../images/planets/yavin-4.png';
+
+const planetsCard = {
+  Alderaan,
+  Bespin,
+  Coruscant,
+  Dagobah,
+  Endor,
+  Hoth,
+  Kamino,
+  Naboo,
+  Tatooine,
+  Yavin4,
+};
+
+export default planetsCard;
